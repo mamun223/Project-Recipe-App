0226938b08037e359c8ac08c282aa67697d5f57f
Mamood <shabanmamun@gmail.com>
2023-12-26 17:09:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// App.jsx
+
+import React, { useState } from "react";
+import "./App.css";
+import RecipeCreate from "./RecipeCreate";
+import RecipeList from "./RecipeList";
+import RecipeData from "./RecipeData";
 
 function App() {
+  const [recipes, setRecipes] = useState(RecipeData);
+
+  const addRecipe = (newRecipe) => {
+    setRecipes([...recipes, newRecipe]);
+  };
+
+  const deleteRecipe = (index) => {
+    const updatedRecipes = [...recipes];
+    updatedRecipes.splice(index, 1);
+    setRecipes(updatedRecipes);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Delicious Food Recipes</h1>
       </header>
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe} />
+      <RecipeCreate addRecipe={addRecipe} />
     </div>
   );
 }
---
+++ src/RecipeCreate.js
@@ -0,0 +1,115 @@
+// RecipeCreate.jsx
+
+import React, { useState } from "react";
+import "./App.css";
+
+function RecipeCreate({ addRecipe }) {
+  const [recipe, setRecipe] = useState({
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setRecipe({ ...recipe, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Ensure that all required fields are filled
+    if (!recipe.name || !recipe.cuisine || !recipe.photo || !recipe.ingredients || !recipe.preparation) {
+      alert("Please fill in all fields.");
+      return;
+    }
+
+    // Create a new recipe object
+    const newRecipe = {
+      name: recipe.name,
+      cuisine: recipe.cuisine,
+      photo: recipe.photo,
+      ingredients: recipe.ingredients,
+      preparation: recipe.preparation,
+    };
+
+    // Update the state with the new recipe
+    addRecipe(newRecipe);
+
+    // Clear the form
+    setRecipe({
+      name: "",
+      cuisine: "",
+      photo: "",
+      ingredients: "",
+      preparation: "",
+    });
+  };
+
+  return (
+    <form name="create" onSubmit={handleSubmit}>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <input
+                type="text"
+                name="name"
+                value={recipe.name}
+                onChange={handleChange}
+                required
+                placeholder="name"
+              />
+            </td>
+            <td>
+              <input
+                type="text"
+                name="cuisine"
+                value={recipe.cuisine}
+                onChange={handleChange}
+                required
+                placeholder="cuisine"
+              />
+            </td>
+            <td>
+              <input
+                type="url"
+                name="photo"
+                value={recipe.photo}
+                onChange={handleChange}
+                required
+                placeholder="url"
+              />
+            </td>
+            <td>
+              <textarea
+                name="ingredients"
+                value={recipe.ingredients}
+                onChange={handleChange}
+                rows="4"
+                required
+                placeholder="ingredients"
+              />
+            </td>
+            <td>
+              <textarea
+                name="preparation"
+                value={recipe.preparation}
+                onChange={handleChange}
+                rows="4"
+                required
+                placeholder="preparation"
+              />
+            </td>
+            <td>
+              <button type="submit">Create</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  );
+}
+
+export default RecipeCreate;
